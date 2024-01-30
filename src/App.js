19b1b7cdf0cd78f1e086b91f20dc18937d6e8b70
Siva Ganesh Manyam <siva.manyam@sprinklr.com>
2024-01-30 21:59:26 +0530

    buildspec added

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React.js now
+          Hello Docker!
         </a>
       </header>
     </div>
